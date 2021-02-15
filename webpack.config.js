f96d17402d61435c955429f54bf1bb5b9c5c8929
Kristoffer Dalby <kradalby@kradalby.no>
2021-02-15 20:48:09 +0000

    Prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,60 @@
-const path = require('path');
-const webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: [
-      './src/index.js'
-    ]
+    app: ["./src/index.js"],
   },
 
   output: {
-    path: path.resolve(__dirname + '/dist'),
-    filename: '[name].js'
+    path: path.resolve(__dirname + "/dist"),
+    filename: "[name].js",
   },
 
   module: {
     rules: [
       {
         test: /\.(css|scss)$/,
-        loaders: [
-          'style-loader', 'css-loader',
-        ]
+        loaders: ["style-loader", "css-loader"],
       },
       {
         test: /\.html$/,
         exclude: /node_modules/,
-        loader: 'file-loader?name=[name].[ext]'
+        loader: "file-loader?name=[name].[ext]",
       },
       {
         test: /\.elm$/,
         exclude: [/elm-stuff/, /node_modules/],
-        loader: 'elm-webpack-loader'
+        loader: "elm-webpack-loader",
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+        loader: "url-loader?limit=10000&mimetype=application/font-woff",
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader'
+        loader: "file-loader",
       },
     ],
 
-    noParse: /\.elm$/
+    noParse: /\.elm$/,
   },
 
   plugins: [
     new CopyWebpackPlugin([
-      { from: 'api', to: 'api' },
-      { from: 'src/assets', to: 'assets' },
+      { from: "api", to: "api" },
+      { from: "src/assets", to: "assets" },
     ]),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false,
       },
-    })
+    }),
   ],
 
   devServer: {
     inline: true,
-    stats: { colors: true }
-  }
+    stats: { colors: true },
+  },
 };
